d51a3c297528c3015362e4005d3affb95c69d3ba
Karllo98 <kaaaroll1998@gmail.com>
2021-06-29 22:00:48 +0200

    Added NewMedicationActivity, activity_main.xml, added listener to buttonAdd

--- app/src/main/java/pl/edu/pwr/s249317/organizer/MainActivity.java
+++ app/src/main/java/pl/edu/pwr/s249317/organizer/MainActivity.java
@@ -2,13 +2,33 @@ package pl.edu.pwr.s249317.organizer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonNext, buttonAdd, buttonYourMedications;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialazeViews();
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewMedicationActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initialazeViews(){
+        buttonNext = findViewById(R.id.buttonNext);
+        buttonAdd = findViewById(R.id.buttonAdd);
+        buttonYourMedications = findViewById(R.id.buttonYourMedications);
     }
 }
